d62e91968489b1df1611e89cca7019113714386e
djordje <djordjesteva@gmail.com>
2019-02-16 17:54:58 +0100

    corrections according to design

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var scrollTop = 0;
+    $(window).scroll(function(){
+      scrollTop = $(window).scrollTop();
+       $('.counter').html(scrollTop);
+      
+      if (scrollTop >= 100) {
+        $('.drive-nav').addClass('scrolled-nav');
+      } else if (scrollTop < 100) {
+        $('.drive-nav').removeClass('scrolled-nav');
+      } 
+      
+    }); 
+    
+  });
